d788842632df7e96b5cd567536c66553e58df584
hzw <194038028@qq.com>
2021-09-16 01:07:27 +0800

    f1 2 commit

--- gittest/src/main/java/byg/hzw/Test.java
+++ gittest/src/main/java/byg/hzw/Test.java
@@ -3,6 +3,7 @@ package byg.hzw;
 public class Test {
     public static void main(String[] args) {
         System.out.println(222);
+        System.out.println(22.2);
         System.out.println("1111122211111111 ");
         
         System.out.println(333);
