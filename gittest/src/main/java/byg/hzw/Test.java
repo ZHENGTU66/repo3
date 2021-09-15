1b04fbbb25e8ccfe468bd525e41a4020cd86b0b0
hzw <194038028@qq.com>
2021-09-16 01:07:55 +0800

    f1 3 commit

--- gittest/src/main/java/byg/hzw/Test.java
+++ gittest/src/main/java/byg/hzw/Test.java
@@ -4,6 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println(222);
         System.out.println(22.2);
+        System.out.println(22.3);
         System.out.println("1111122211111111 ");
         
         System.out.println(333);
